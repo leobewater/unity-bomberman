d88e29cf641bbd630e505cc0cee301da7328c0c4
leo <mmbee888@gmail.com>
2023-04-02 15:55:34 -0400

    add animation sprite renderer script

---
+++ Assets/Scripts/AnimatedSpriteRenderer.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatedSpriteRenderer : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+
+    public Sprite idleSprite;
+    public Sprite[] animationSprites;
+
+    public float animationTime = 0.25f;
+    private int animationFrame; // Keep track which animation frame
+
+    // Loop animation and is animation idle
+    public bool loop = true;
+    public bool idle = true;
+
+    private void Awake() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnEnable() {
+        // Control the sprite render when game object is enabled
+        spriteRenderer.enabled = true;
+    }
+
+    private void OnDisable() {
+        spriteRenderer.enabled = false;
+    }
+
+    private void Start() {
+        InvokeRepeating(nameof(NextFrame), animationTime, animationTime);
+    }
+
+    private void NextFrame() {
+        animationFrame++;
+        if (loop && animationFrame >= animationSprites.Length) {
+            animationFrame = 0;
+        }
+
+        if (idle) {
+            // Set idle sprite
+            spriteRenderer.sprite = idleSprite;
+        } else if (animationFrame >= 0 && animationFrame < animationSprites.Length) {
+            // Not idle and within array bound, set the animation sprite from array
+            spriteRenderer.sprite = animationSprites[animationFrame];
+        }
+    }
+}
