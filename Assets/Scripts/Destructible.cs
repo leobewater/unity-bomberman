36ed69ed5f38c50ff33fe5187603693478056b70
leo <mmbee888@gmail.com>
2023-04-02 17:30:09 -0400

    assign items to destructible spawnable items array

--- Assets/Scripts/Destructible.cs
+++ Assets/Scripts/Destructible.cs
@@ -6,6 +6,11 @@ public class Destructible : MonoBehaviour
 {
     public float destructionTime = 1f;
 
+    // 20% chance to spawn an item after the brick is destroyed
+    [Range(0, 1)]
+    public float itemSpawnChance = 0.2f;
+    public GameObject[] spawnableItems;
+
     private void Start() {
         Destroy(gameObject, destructionTime);
     }
