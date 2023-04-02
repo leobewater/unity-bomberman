6ac21d9d836f32ee2b46d891398e4cfd6bba9e2b
leo <mmbee888@gmail.com>
2023-04-02 16:43:15 -0400

    add explosion animation logic

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    public AnimatedSpriteRenderer start;
+    public AnimatedSpriteRenderer middle;
+    public AnimatedSpriteRenderer end;
+
+    public void SetActiveRenderer(AnimatedSpriteRenderer renderer) {
+        start.enabled = renderer == start;
+        middle.enabled = renderer == middle;
+        end.enabled = renderer == end;
+    }
+
+    public void SetDirection(Vector2 direction) {
+        // Rotate the explosion sprites based on direction
+        float angle = Mathf.Atan2(direction.y, direction.x);
+        // Rotate the Z axis (Vector3.forward)
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+}
